d38d33dbd88dcf162bbc44558e97b42ef32c5696
WeAreAngus <52379007+hackermanangus@users.noreply.github.com>
2020-11-14 14:06:24 +0100

    Adding system 70% complete

--- src/royalroad.rs
+++ src/royalroad.rs
@@ -5,39 +5,61 @@ use std::error::Error;
 use regex::Regex;
 use sqlx::Executor;
 
-struct Novel {
-    novel_id: String,
-    chapter_id: String
-}
 
 pub async fn handle(msg: Box<MessageCreate>, bot: Arc<Bot>, args: Vec<&str>) -> Result<(), Box<dyn Error + Send + Sync>> {
     match args[1] {
         "add" => {
+            println!("{:?}", &args);
             let mut db = &mut bot.pool.acquire().await.unwrap();
-            let query = sqlx::query_as!(Novel,"SELECT * FROM Novels WHERE novel_id=?", args[1] )
-                .fetch_one(&mut db)
-                .await?;
+            let query = sqlx::query("SELECT * FROM Novels WHERE novel_id=?")
+                .bind(args[2])
+                .execute(&mut db).await;
             let guild_id = msg.guild_id.unwrap().to_string();
             let channel_id = msg.channel_id.to_string();
 
-            let check = match query {
+            match query {
+                Ok(inner) => {
+                    match inner {
+                        1 => {
+                        sqlx::query("DELETE FROM Guilds WHERE guild_id=? AND channel_id=? AND novel_id=?")
+                        .bind(&guild_id)
+                        .bind(&channel_id)
+                        .bind(&args[2])
+                        .execute(&mut db).await ?;
+                        sqlx::query("INSERT INTO Guilds (guild_id , channel_id, novel_id) VALUES (?, ?, ?)")
+                        .bind(guild_id)
+                        .bind(channel_id)
+                        .bind(args[2])
+                        .execute(&mut db).await ?;
+                        &bot.http.create_message(msg.channel_id).content("Added your guild to the list!") ?.await ?;
+                        return Ok(())
+                    },
+                        0 => {
+                            let result = reqwest::get(args[2]).await;
+                            let result = match result {
+                                Ok(body) => body.text().await?,
+                                Err(e) => {
+                                    bot.http.create_message(msg.channel_id).content("Unable to find fiction")?.await?;
+                                    return Ok(())
+                                }
+                            };
+                            // <td>.*?<a[^<>]*href=["'](?P<chapter_link>[^"']+)["'] regex flor made
+                            let re = Regex::new(r#"(?sgm)<td>.*?<a[^<>]*href=["'](?P<chapter_link>[^"']+)["']"#).unwrap();
 
-                Some(_) => { sqlx::query!("UPDATE Guilds SET guild_id=?, channel_id=?, novel_id=?", (guild_id, channel_id, args[2])) },
+                            for results in re.captures_iter(&result) {
+                                println!("{:?}", &results["chapter_link"] )
+                            }
+                        },
+                        _ => {}
+                    }
 
-
-            };
-            let result = reqwest::get(args[2]).await;
-            let result = match result {
-                Ok(body) => {},
-                Err(e) => {
-                    bot.http.create_message(msg.channel_id).content("Unable to find fiction")?.await?;
+                },
+                Err(_) => {
+                    &bot.http.create_message(msg.channel_id).content("Something went wrong, the dev has been notified!")?.await?;
                     return Ok(())
                 }
             };
-            // <td>.*?<a[^<>]*href=["'](?P<chapter_link>[^"']+)["'] regex flor made
-            let re = Regex::new(r#"(?sgm)<td>.*?<a[^<>]*href=["'](?P<chapter_link>[^"']+)["']/sgm"#).unwrap();
 
-            let regex_result = re.captures_iter(&result);
         }
         "remove" => {
 

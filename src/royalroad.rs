43f7e90d89df80c9542baf64564d73e4ec71a77a
WeAreAngus <52379007+hackermanangus@users.noreply.github.com>
2020-11-14 16:15:28 +0100

    Add command of the royalroad module is 100% done

--- src/royalroad.rs
+++ src/royalroad.rs
@@ -1,10 +1,10 @@
-use twilight_model::gateway::payload::MessageCreate;
-use std::sync::Arc;
-use crate::Bot;
 use std::error::Error;
-use regex::Regex;
-use sqlx::Executor;
+use std::sync::Arc;
 
+use regex::Regex;
+use twilight_model::gateway::payload::MessageCreate;
+
+use crate::Bot;
 
 pub async fn handle(msg: Box<MessageCreate>, bot: Arc<Bot>, args: Vec<&str>) -> Result<(), Box<dyn Error + Send + Sync>> {
     match args[1] {
@@ -21,51 +21,83 @@ pub async fn handle(msg: Box<MessageCreate>, bot: Arc<Bot>, args: Vec<&str>) -> 
                 Ok(inner) => {
                     match inner {
                         1 => {
-                        sqlx::query("DELETE FROM Guilds WHERE guild_id=? AND channel_id=? AND novel_id=?")
-                        .bind(&guild_id)
-                        .bind(&channel_id)
-                        .bind(&args[2])
-                        .execute(&mut db).await ?;
-                        sqlx::query("INSERT INTO Guilds (guild_id , channel_id, novel_id) VALUES (?, ?, ?)")
-                        .bind(guild_id)
-                        .bind(channel_id)
-                        .bind(args[2])
-                        .execute(&mut db).await ?;
-                        &bot.http.create_message(msg.channel_id).content("Added your guild to the list!") ?.await ?;
-                        return Ok(())
-                    },
+                            sqlx::query("DELETE FROM Guilds WHERE guild_id=? AND channel_id=? AND novel_id=?")
+                                .bind(&guild_id)
+                                .bind(&channel_id)
+                                .bind(&args[2])
+                                .execute(&mut db).await?;
+                            sqlx::query("INSERT INTO Guilds (guild_id , channel_id, novel_id) VALUES (?, ?, ?)")
+                                .bind(guild_id)
+                                .bind(channel_id)
+                                .bind(args[2])
+                                .execute(&mut db).await?;
+                            &bot.http.create_message(msg.channel_id).content("Added your guild to the list!")?.await?;
+                            return Ok(());
+                        }
                         0 => {
                             let result = reqwest::get(args[2]).await;
                             let result = match result {
                                 Ok(body) => body.text().await?,
-                                Err(e) => {
-                                    bot.http.create_message(msg.channel_id).content("Unable to find fiction")?.await?;
-                                    return Ok(())
+                                Err(_) => {
+                                    &bot.http.create_message(msg.channel_id).content("Unable to find fiction")?.await?;
+                                    return Ok(());
                                 }
                             };
                             // <td>.*?<a[^<>]*href=["'](?P<chapter_link>[^"']+)["'] regex flor made
-                            let re = Regex::new(r#"(?sgm)<td>.*?<a[^<>]*href=["'](?P<chapter_link>[^"']+)["']"#).unwrap();
-
-                            for results in re.captures_iter(&result) {
-                                println!("{:?}", &results["chapter_link"] )
+                            // TODO: <meta name="description" content="(?P<description>[^">]*)["][>] regex I made big brain
+                            // TODO: \/chapter\/(?P<chapter_id>[0-9]*)\/ another regex I wrote to get the id
+                            let re = Regex::new(r#"(?sm)<td>.*?<a[^<>]*href=["'](?P<chapter_link>[^"']+)["']"#).unwrap();
+                            let re_c_id = Regex::new(r#"/chapter/(?P<chapter_id>[0-9]*)/"#).unwrap();
+                            let mut truth: bool = true;
+                            let mut temp: String = String::new();
+                            for capture in re.captures_iter(&result)
+                            {
+                                let one = re_c_id.captures(&capture["chapter_link"]);
+                                if let Some(r) = one {
+                                    println!("{}", &r["chapter_id"]);
+                                    temp.push_str(&r["chapter_id"]);
+                                    temp.push_str(" ");
+                                } else {
+                                    truth = false;
+                                }
                             }
-                        },
+                            return if truth {
+                                println!("{:?}", &temp);
+                                sqlx::query("INSERT INTO Novels (novel_id, chapter_id) VALUES (?, ?)")
+                                    .bind(args[2])
+                                    .bind(temp)
+                                    .execute(&mut db).await?;
+                                sqlx::query("DELETE FROM Guilds WHERE guild_id=? AND channel_id=? AND novel_id=?")
+                                    .bind(&guild_id)
+                                    .bind(&channel_id)
+                                    .bind(&args[2])
+                                    .execute(&mut db).await?;
+                                sqlx::query("INSERT INTO Guilds (guild_id , channel_id, novel_id) VALUES (?, ?, ?)")
+                                    .bind(guild_id)
+                                    .bind(channel_id)
+                                    .bind(args[2])
+                                    .execute(&mut db).await?;
+                                &bot.http.create_message(msg.channel_id).content("Added new fiction to the database!")?.await?;
+                                Ok(())
+                            } else {
+                                &bot.http.create_message(msg.channel_id).content("Invalid fiction provided")?.await?;
+                                Ok(())
+                            }
+                        }
                         _ => {}
                     }
-
-                },
+                }
                 Err(_) => {
                     &bot.http.create_message(msg.channel_id).content("Something went wrong, the dev has been notified!")?.await?;
-                    return Ok(())
+                    return Ok(());
                 }
             };
-
         }
         "remove" => {
-
+            todo!("Do this after add, shouldn't be too hard")
         }
         "check" => {
-
+            todo!("This is not important, leave it for last")
         }
         _ => {}
     }

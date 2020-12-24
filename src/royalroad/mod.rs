3a040bd8bd54ff0a7d8790bb6fa716789aae0de5
WeAreAngus <52379007+hackermanangus@users.noreply.github.com>
2020-12-24 18:10:30 +0100

    Added embeds

--- src/royalroad/mod.rs
+++ src/royalroad/mod.rs
@@ -12,23 +12,44 @@ use crate::Bot;
 use crate::royalroad::add::add;
 use crate::royalroad::remove::remove;
 use crate::royalroad::check::check;
+use twilight_embed_builder::{EmbedBuilder, EmbedFieldBuilder};
+use chrono::{Utc, SecondsFormat};
 
 pub async fn handle(msg: Box<MessageCreate>, bot: Arc<Bot>, args: Vec<&str>) -> Result<(), Box<dyn Error + Send + Sync>> {
+    let embed = EmbedBuilder::new()
+        .title("Result")?
+        .timestamp(Utc::now().to_rfc3339_opts(SecondsFormat::Millis, true))
+        .color(0x80_00_80)?;
+
     match args[1] {
         "add" => {
-            let message = add(msg.clone(), bot.clone(), args).await?;
-            &bot.http.create_message(msg.channel_id).content(message.0)?.await?;
+            if args.len() <3 {
+                let embed = embed.description("`EmptyArgument`: No provided arguments")?.field(EmbedFieldBuilder::new("Usage", ">royalroad add <link>")?).build()?;
+                &bot.http.create_message(msg.channel_id).embed(embed)?.await?;
+            } else {
+                let message = add(msg.clone(), bot.clone(), args).await?;
+                let embed = embed.description(message.0)?.build()?;
+                &bot.http.create_message(msg.channel_id).embed(embed)?.await?;
+            }
         }
         "remove" => {
-            let message = remove(msg.clone(), bot.clone(), args).await?;
-            &bot.http.create_message(msg.channel_id).content(message.0)?.await?;
+            if args.len() < 3 {
+                let embed = embed.description("`EmptyArgument`: No provided arguments")?.field(EmbedFieldBuilder::new("Usage", ">royalroad remove <link>")?).build()?;
+                &bot.http.create_message(msg.channel_id).embed(embed)?.await?;
+            } else {
+                let message = remove(msg.clone(), bot.clone(), args).await?;
+                let embed = embed.description(message.0)?.build()?;
+                &bot.http.create_message(msg.channel_id).embed(embed)?.await?;
+            }
         }
         "check" => {
             let message = check(msg.clone(), bot.clone()).await?;
-            &bot.http.create_message(msg.channel_id).content(message.0)?.await?;
+            let embed = embed.description(message.0)?.build()?;
+            &bot.http.create_message(msg.channel_id).embed(embed)?.await?;
         }
         _ => {
-            &bot.http.create_message(msg.channel_id).content("Unknown sub-command")?.await?;
+            let embed = embed.description("`Unknown subcommand`")?.build()?;
+            &bot.http.create_message(msg.channel_id).embed(embed)?.await?;
         }
     }
     Ok(())
